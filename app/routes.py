3443544e09f71a40232bf0571139d554d362bcf6
Joshua Kruger <jkruger2@ithaca.edu>
2021-11-08 13:34:15 -0500

    Home page

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,22 @@ from app import app
 
 @app.route('/')
 def home():
+    welcomeMessage = "Welcome to Call of Duty stat tracker!"
 
-    return render_template('base.html', title="Welcome to COD Tracker!")
+
+    return render_template('greeting.html', message=welcomeMessage)
+
+@app.route('/leaderboards')
+def leaderboards():
+
+    return render_template('leaderboards.html', title="This is the leaderboards page")
+
+@app.route('/login')
+def login():
+
+    return render_template('login.html', title="This is the login page")
+
+@app.route('/register')
+def register():
+
+    return render_template('register.html', title="This is the register page")
