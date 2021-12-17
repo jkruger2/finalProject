88321eefd0c71dcf09d9a175e98fef18a8f5eb91
Muhta3 <89270569+Muhta3@users.noreply.github.com>
2021-12-17 05:17:14 -0500

    Initial API connection

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
 import jinja2.exceptions
-from flask import render_template, flash, redirect, url_for
-from app import app
+import sqlalchemy.exc
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
 from app.forms import LoginForm, RegistrationForm, StatSearchForm
+from flask_login import login_user, logout_user, current_user, login_required
+from app.models import User
 import http.client
 import json
 
 
 @app.route('/leaderboards', methods=['GET'])
+@login_required
 def leaderboards():
     conn = http.client.HTTPSConnection("call-of-duty-modern-warfare.p.rapidapi.com")
     headers = {
@@ -24,6 +28,7 @@ def leaderboards():
 
 
 @app.route('/statSearch', methods=['GET', 'POST'])
+@login_required
 def statSearch():
     try:
         form = StatSearchForm()
@@ -58,20 +63,51 @@ def statSearch():
 def home():
     welcomeMessage = "Welcome to Call of Duty stat tracker!"
 
-
     return render_template('greeting.html', message=welcomeMessage)
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
-
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
+    try:
+        if form.validate_on_submit():
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Congratulations, you are now a registered user!')
+            return redirect(url_for('login'))
 
-    return render_template('register.html', form=form)
+    except sqlalchemy.exc.IntegrityError:
+        flash("Already Registered user!")
+        redirect(url_for('register'))
+
+        return render_template('register.html', username=username, email=email)
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
